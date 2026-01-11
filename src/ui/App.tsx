62275ee5cffb322a381cbc0ea7dee8a7800937de
Faisal Husain <faisalhusain1320@gmail.com>
2026-01-11 19:34:05 +0530

    done : now i have data now need to send to ui

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -12,7 +12,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + Reactasdfasdf</h1>
+      <h1>Faisal HJusai</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
